87a81d8950e009a31780b7a35d3ee2039ffc4796
MANOJ-51 <m4anoj51@gmail.com>
2022-06-04 17:51:54 +0530

    added:stack and bet variables

--- com/gambling/simulation/GamblingSimulation.java
+++ com/gambling/simulation/GamblingSimulation.java
@@ -1,6 +1,8 @@
 package com.gambling.simulation;
 
 public class GamblingSimulation {
+    static int accountBalance=100;      //palyer account balance
+    static final int BET=1;             //player bets 1 for every single match
     public static void main(String[] args) {
         //displaying welcome message
         System.out.println("WELCOME TO GAMBLING SIMULATION PROGRAM");
