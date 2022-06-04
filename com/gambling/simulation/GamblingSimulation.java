8bb3532f539fb771e775243577ecb0242a186232
MANOJ-51 <m4anoj51@gmail.com>
2022-06-04 17:42:35 +0530

    displaying welcome message

---
+++ com/gambling/simulation/GamblingSimulation.java
@@ -0,0 +1,9 @@
+package com.gambling.simulation;
+
+public class GamblingSimulation {
+    public static void main(String[] args) {
+        //displaying welcome message
+        System.out.println("WELCOME TO GAMBLING SIMULATION PROGRAM");
+    }
+
+}
