45959ca063f95ef7f9fe62b9a76e700eb609cba9
MANOJ-51 <m4anoj51@gmail.com>
2022-06-04 22:53:08 +0530

    added: luckyday and unluckyday

--- com/gambling/simulation/GamblingSimulation.java
+++ com/gambling/simulation/GamblingSimulation.java
@@ -3,10 +3,17 @@ package com.gambling.simulation;
 public class GamblingSimulation {
     static final int BET = 1;             //player bets 1 for every single match
     static int accountBalance = 100;      //palyer account balance
-    static int numberOfWinsDay =0;
-    static int numberOfLossDay=0;
-    static int numberOfWinsMonth=0;
-    static int numberOfLossMonth=0;
+    static int numberOfWinsDay = 0;
+    static int numberOfLossDay = 0;
+    static int numberOfWinsMonth = 0;
+    static int numberOfLossMonth = 0;
+
+    //temporary variable for uc-6
+    static int day = 0;
+    static int previousDayWin = 0;
+    static int previousDayLoss = 0;
+    static int luckyDAY;
+    static int unluckyDay;
 
     //uc-2 as the gambler plays with 1 either he wins 1 or he losses 1 .
     public static void winOrLoose() {
@@ -41,23 +48,41 @@ public class GamblingSimulation {
                 break;
             }
         }
-        System.out.println("GAMBLERS ACCOUNT BALANCE IS : " +accountBalance);
+        System.out.println("GAMBLERS ACCOUNT BALANCE IS : " + accountBalance);
         System.out.println("NUMBER OF WINS IN A DAY IS : " + numberOfWinsDay);
-        System.out.println("NUMBER OF LOSSES IN A DAY IS : "+numberOfLossDay);
+        System.out.println("NUMBER OF LOSSES IN A DAY IS : " + numberOfLossDay);
+
+        luckyAndUnlucky(); //calling uc-6 here
+
         //we are making wins and loss in day =0 and accouunt balence =100; for a new day
-        numberOfWinsDay=0;
-        numberOfLossDay=0;
-        accountBalance=100;
+        numberOfWinsDay = 0;
+        numberOfLossDay = 0;
+        accountBalance = 100;
 
     }
+
     //uc-4 after onr month of bet  wons and loss
-    public static void gamblingForMonth(){
-        for (int day=1;day<30;day++){
-            System.out.println("DAY:"+day+":-");
+    public static void gamblingForMonth() {
+        for (day = 1; day <= 30; day++) {
+            System.out.println("DAY:" + day + ":-");
             gambling();
         }
-        System.out.println("TOTAL NUMBER OF WINS IN A MONTH : "+numberOfWinsMonth);
-        System.out.println("TOTAL NUMBER OF LOSS IN A MONTH : "+numberOfLossMonth);
+        System.out.println("LUCKYDAY IS : " + luckyDAY);
+        System.out.println("UNLUCKYDAY IS :" + unluckyDay);
+        System.out.println("TOTAL NUMBER OF WINS IN A MONTH : " + numberOfWinsMonth);
+        System.out.println("TOTAL NUMBER OF LOSS IN A MONTH : " + numberOfLossMonth);
+    }
+
+    //uc-6 luckyday and unlucky day
+    public static void luckyAndUnlucky() {
+        if (previousDayWin < numberOfWinsDay) {           //finding day max wins
+            previousDayWin = numberOfWinsDay;
+            luckyDAY = day;
+        }
+        if (previousDayLoss < numberOfLossDay) {          //finding day max loss
+            previousDayLoss = numberOfLossDay;
+            unluckyDay = day;
+        }
     }
 
     public static void main(String[] args) {
